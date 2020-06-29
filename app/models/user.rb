9b8a4e87a3885871cab5d37df5a33983ec47fe68
EC2 Default User <ec2-user@ip-172-31-6-255.ap-northeast-1.compute.internal>
2020-06-29 10:36:01 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+
+   has_many  :books, dependent: :destroy
+
+   validates :name, presence: true,length: {in: 2..20}
+   validates :introduction,length: { maximum: 50}
+
+   attachment :profile_image
+ # deviseでemailを不必要にする
+  def email_required?
+   false
+  end
+  def email_changed?
+   false
+  end
+
+
+end
+
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '80d1fcb7c9bae51b1923b101960f235c5cb769af7d9a8eb70a9f36873a6fa7d50d1b38b46f2af047259f6935bfe1087365521d1351c6d7181d64cc7347230f51'
